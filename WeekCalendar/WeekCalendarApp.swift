f78e8550d7c0c5241dabe5761012f9cbd135e9e1
David Diego Gomez <diegodavid@icloud.com>
2023-06-18 12:17:28 -0300

    first commit

---
+++ WeekCalendar/WeekCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeekCalendarApp.swift
+//  WeekCalendar
+//
+//  Created by David Gomez on 18/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeekCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
